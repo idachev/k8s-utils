41e5d9631357d03873094ba95501d9016792e4d1
Ivan Dachev <i_dachev@yahoo.co.uk>
2023-01-24 07:07:00 +0200

    Enhance utils

--- k8s-remote-exec.sh
+++ k8s-remote-exec.sh
@@ -8,5 +8,4 @@ if [[ -z "${POD_NAME}" ]]; then
   POD_NAME=${IGD_UTILS_POD_NAME}
 fi
 
-kubectl run ${POD_NAME} --labels="name=${POD_NAME}" \
-  --image=${IGD_UTILS_DOCKER_REMOTE_REGISTRY}/${IGD_UTILS_DOCKER_IMG}:${IGD_UTILS_DOCKER_TAG}
+kubectl exec -i -t "${POD_NAME}" -c "${POD_NAME}" -- bash -c "$@"
--- k8s-remove.sh
+++ k8s-remove.sh
@@ -4,8 +4,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 source ${DIR}/igd-utils-lib.sh
 
-POD_NAME=$1
-
 if [[ -z "${POD_NAME}" ]]; then
   POD_NAME=${IGD_UTILS_POD_NAME}
 fi
